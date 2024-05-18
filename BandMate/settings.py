e19455d05b56af2ef2a21f80a31f283e01cb5616
Pocverdn <ssanchezc5@eafit.edu.co>
2024-05-18 18:24:55 -0500

    Nueva id externa

--- BandMate/settings.py
+++ BandMate/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure--^30-j24o!^979&#-b3fw%nqzwn@0+-)pzb*gql(1cl5r(j4s#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.128.0.2']
+ALLOWED_HOSTS = ['34.136.116.146']
 
 
 # Application definition
