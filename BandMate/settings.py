dc8f37fd3dd0f675f5abff65fec3975d4214bf0b
Pocverdn <ssanchezc5@eafit.edu.co>
2024-05-22 14:48:19 -0500

    new key 2

--- BandMate/settings.py
+++ BandMate/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure--^30-j24o!^979&#-b3fw%nqzwn@0+-)pzb*gql(1cl5r(j4s#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.223.128.63']
+ALLOWED_HOSTS = ['34.29.134.148']
 
 
 # Application definition
